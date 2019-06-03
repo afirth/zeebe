749b7adba2384043dd52b6fd5bbf2b553f7faf55
Miguel Pires <miguel.pires@camunda.com>
2019-06-03 17:12:58 +0200

    chore(engine): migrated ActivateJobsTest

--- engine/src/test/java/io/zeebe/engine/util/EngineRule.java
+++ engine/src/test/java/io/zeebe/engine/util/EngineRule.java
@@ -33,6 +33,7 @@ import io.zeebe.engine.processor.workflow.message.command.SubscriptionCommandSen
 import io.zeebe.engine.state.DefaultZeebeDbFactory;
 import io.zeebe.exporter.api.record.Record;
 import io.zeebe.exporter.api.record.value.DeploymentRecordValue;
+import io.zeebe.exporter.api.record.value.JobRecordValue;
 import io.zeebe.exporter.api.record.value.WorkflowInstanceRecordValue;
 import io.zeebe.exporter.api.record.value.deployment.ResourceType;
 import io.zeebe.logstreams.impl.Loggers;
@@ -45,9 +46,11 @@ import io.zeebe.protocol.Protocol;
 import io.zeebe.protocol.impl.record.RecordMetadata;
 import io.zeebe.protocol.impl.record.UnifiedRecordValue;
 import io.zeebe.protocol.impl.record.value.deployment.DeploymentRecord;
+import io.zeebe.protocol.impl.record.value.job.JobRecord;
 import io.zeebe.protocol.impl.record.value.workflowinstance.WorkflowInstanceCreationRecord;
 import io.zeebe.protocol.impl.record.value.workflowinstance.WorkflowInstanceRecord;
 import io.zeebe.protocol.intent.DeploymentIntent;
+import io.zeebe.protocol.intent.JobIntent;
 import io.zeebe.protocol.intent.WorkflowInstanceCreationIntent;
 import io.zeebe.protocol.intent.WorkflowInstanceIntent;
 import io.zeebe.test.util.record.RecordingExporter;
@@ -56,6 +59,7 @@ import io.zeebe.util.ReflectUtil;
 import io.zeebe.util.buffer.BufferWriter;
 import io.zeebe.util.sched.ActorCondition;
 import io.zeebe.util.sched.ActorControl;
+import io.zeebe.util.sched.clock.ControlledActorClock;
 import io.zeebe.util.sched.future.ActorFuture;
 import io.zeebe.util.sched.future.CompletableActorFuture;
 import java.time.Duration;
@@ -139,6 +143,10 @@ public class EngineRule extends ExternalResource {
     environmentRule.getClock().addTime(duration);
   }
 
+  public ControlledActorClock getClock() {
+    return environmentRule.getClock();
+  }
+
   public Record<DeploymentRecordValue> deploy(final BpmnModelInstance modelInstance) {
     final DeploymentRecord deploymentRecord = new DeploymentRecord();
     deploymentRecord
@@ -215,6 +223,20 @@ public class EngineRule extends ExternalResource {
 
   public VariableClient variables() {
     return new VariableClient(environmentRule);
+  }
+
+  public long completeJob(long jobKey, DirectBuffer variables) {
+    final JobRecord jobRecord = new JobRecord().setVariables(variables);
+    return environmentRule.writeCommand(jobKey, JobIntent.COMPLETE, jobRecord);
+  }
+
+  public Record<JobRecordValue> completeJobAndWait(long jobKey, DirectBuffer variables) {
+    final long position = completeJob(jobKey, variables);
+    return RecordingExporter.jobRecords()
+        .filter(r -> r.getPosition() > position)
+        .withRecordKey(jobKey)
+        .withIntent(JobIntent.COMPLETED)
+        .getFirst();
   }
 
   public JobActivationClient jobs() {
